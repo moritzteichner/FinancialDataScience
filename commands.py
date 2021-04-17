acee35a95e56292ff5ec7a13eb9b7e769558dd3d
Moritz Teichner <uprcw@student.kit.edu>
2021-04-17 15:34:28 +0200

    Added another utility and marginal utility function. Added a legend and a label for the x axis.

--- commands.py
+++ commands.py
@@ -7,17 +7,28 @@ def utility_function(wealth, gamma):
     if gamma == 1:
         return np.log(wealth)
     elif gamma >= 0:
-        return (wealth ** float(1 - gamma) - 1) / (1 - gamma)
+        return (wealth ** (1 - gamma) - 1) / (1 - gamma)
+
 
 def marginal_utility_function(wealth, gamma):
     if gamma >= 0:
-        return (1 / wealth ** gamma)
+        return 1 / wealth ** gamma
 
-x_axis = np.arange(1, 999999, 10000)
-print(x_axis)
-curve = utility_function(x_axis, 0.6)
-marginal_curve = marginal_utility_function(x_axis, 0.6)
 
-pp.plot(x_axis, curve)
-pp.plot(x_axis, marginal_curve)
+x_axis = np.arange(1, 999999, 1000)
+
+curve1 = utility_function(x_axis, 0.8)
+marginal_curve1 = marginal_utility_function(x_axis, 0.8)
+
+curve2 = utility_function(x_axis, 1)
+marginal_curve2 = marginal_utility_function(x_axis, 1)
+
+pp.plot(x_axis, curve1)
+pp.plot(x_axis, marginal_curve1)
+
+pp.plot(x_axis, curve2)
+pp.plot(x_axis, marginal_curve2)
+
+pp.legend(["U(WT), Gamma=0.8", "U'(WT), Gamma=0.8", "U(WT), Gamma=1", "U'(WT), Gamma=1"])
+pp.xlabel("W_T")
 pp.show()
