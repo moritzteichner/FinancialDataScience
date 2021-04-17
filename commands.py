0682541de4c5de301e0f9981e6ae99dbaebdded3
Moritz Teichner <uprcw@student.kit.edu>
2021-04-17 15:13:15 +0200

    Added the utility and marginal utility function from assignment 1. Plotted the two graphs.

--- commands.py
+++ commands.py
@@ -1,3 +1,23 @@
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as pp
 
-print("Test")
+
+def utility_function(wealth, gamma):
+    if gamma == 1:
+        return np.log(wealth)
+    elif gamma >= 0:
+        return (wealth ** float(1 - gamma) - 1) / (1 - gamma)
+
+def marginal_utility_function(wealth, gamma):
+    if gamma >= 0:
+        return (1 / wealth ** gamma)
+
+x_axis = np.arange(1, 999999, 10000)
+print(x_axis)
+curve = utility_function(x_axis, 0.6)
+marginal_curve = marginal_utility_function(x_axis, 0.6)
+
+pp.plot(x_axis, curve)
+pp.plot(x_axis, marginal_curve)
+pp.show()
