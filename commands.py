328b97bd83c846fdd8d00323261633678b16b66d
Moritz Teichner <uprcw@student.kit.edu>
2021-04-18 01:06:37 +0200

    Added color and splitted the graphs in two different plots.

--- commands.py
+++ commands.py
@@ -16,19 +16,30 @@ def marginal_utility_function(wealth, gamma):
 
 
 x_axis = np.arange(1, 999999, 1000)
+x_axis1 = np.arange(1, 500000, 1000)
+x_axis2 = np.arange(500000, 999999, 1000)
 
-curve1 = utility_function(x_axis, 0.8)
+curve1_1 = utility_function(x_axis1, 0.8)
+curve1_2 = utility_function(x_axis2, 0.8)
 marginal_curve1 = marginal_utility_function(x_axis, 0.8)
 
-curve2 = utility_function(x_axis, 1)
-marginal_curve2 = marginal_utility_function(x_axis, 1)
+curve2_1 = utility_function(x_axis1, 0.1)
+curve2_2 = utility_function(x_axis2, 0.1)
+marginal_curve2 = marginal_utility_function(x_axis, 0.1)
 
-pp.plot(x_axis, curve1)
-pp.plot(x_axis, marginal_curve1)
+pp.subplot(1, 2, 1)
+pp.plot(x_axis1, curve1_1, color="red")
+pp.plot(x_axis2, curve1_2, color="green")
 
-pp.plot(x_axis, curve2)
-pp.plot(x_axis, marginal_curve2)
+pp.plot(x_axis1, curve2_1, color="red")
+pp.plot(x_axis2, curve2_2, color="green")
 
-pp.legend(["U(WT), Gamma=0.8", "U'(WT), Gamma=0.8", "U(WT), Gamma=1", "U'(WT), Gamma=1"])
-pp.xlabel("W_T")
+pp.xlabel("W_T in Euros")
+pp.ylabel("U(W_T)")
+
+pp.subplot(1, 2, 2)
+pp.plot(x_axis, marginal_curve1, color="blue")
+pp.plot(x_axis, marginal_curve2, color="black")
+pp.xlabel("W_T in Euros")
+pp.ylabel("U'(W_T)")
 pp.show()
